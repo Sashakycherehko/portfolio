583e721d46d899043da7467da9b8c62381d699cc
SashaKycherehko <=>
2022-08-09 21:17:53 +0300

    Узнал новое

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,17 @@
-const wr = document.querySelector(".navigation");
+const CreateButtonNavigation = () => {
+  let navigation = document.querySelector(".info-navigation");
+  let innerHeight = window.innerHeight;
+  let innerWidth = window.outerWidth;
+  console.log(innerWidth, navigation.getBoundingClientRect());
+  navigation.style = `left: ${innerWidth}px`;
+};
+// document.addEventListener("keypress", (e) => {
+//   e.preventDefault;
+//   switch (e.key) {
+//     case "w":
+//     case "ц":
+//       document.body.classList.toggle("active");
+//   }
+// });
 
-document.addEventListener("keypress", (e) => {
-  e.preventDefault;
-  switch (e.key) {
-    case "w":
-      document.body.classList.toggle("active");
-  }
-});
+window.addEventListener("resize", CreateButtonNavigation);
