99cb6b181c38a34679eff482ba9ed62d386f225f
SashaKycherehko <=>
2022-08-10 17:41:02 +0300

    testing cursor

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,15 @@
-let body = document.body;
+let cursor = document.getElementById("container-cursor");
+let outer_cursor = document.getElementById("container-cursor_outer");
+console.log(cursor);
 
-document.addEventListener("wheel", (e) => {
-  let dir = Math.sign(e.deltaY);
-  console.log(dir);
-});
+const CreateCursor = (e) => {
+  e.preventDefault;
+  cursor.style = `top: ${e.pageY - 5}px;left: ${e.pageX - 5}px`;
+  outer_cursor.style = `top: ${e.pageY - 22.5}px;left: ${e.pageX - 22.5}px`;
+};
+
+const CreateClick = (e) => {
+  e.preventDefault;
+  console.log("work");
+};
+document.addEventListener("mousemove", CreateCursor);
