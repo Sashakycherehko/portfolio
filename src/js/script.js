cbf3a13a27627ed7b2895227bb1841839beaf2cc
SashaKycherehko <=>
2022-08-10 12:43:20 +0300

    neznau

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,6 @@
-const CreateButtonNavigation = () => {
-  let navigation = document.querySelector(".info-navigation");
-  let innerHeight = window.innerHeight;
-  let innerWidth = window.outerWidth;
-  console.log(innerWidth, navigation.getBoundingClientRect());
-  navigation.style = `left: ${innerWidth}px`;
-};
-// document.addEventListener("keypress", (e) => {
-//   e.preventDefault;
-//   switch (e.key) {
-//     case "w":
-//     case "ц":
-//       document.body.classList.toggle("active");
-//   }
-// });
+let body = document.body;
 
-window.addEventListener("resize", CreateButtonNavigation);
+document.addEventListener("wheel", (e) => {
+  let dir = Math.sign(e.deltaY);
+  console.log(dir);
+});
